4e7ff0bd860458527f00c6b32dcd055e9a051da4
bkapil002 <kapilabrahm@gmail.com>
2026-07-15 15:53:16 +0530

    save

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -79,7 +79,7 @@ export default function App() {
     formData.append("video", video);
 
     try {
-      const res = await axios.post("http://localhost:5000/upload", formData, {
+      const res = await axios.post("https://subtitle-backend.samzara.in/upload", formData, {
         onUploadProgress: (e) => {
           const pct = Math.round((e.loaded / e.total) * 50);
           setProgress(pct);
@@ -94,7 +94,7 @@ export default function App() {
         setProgress(Math.round(fake));
       }, 600);
 
-      const fullUrl = `http://localhost:5000${res.data.file}`;
+      const fullUrl = `https://subtitle-backend.samzara.in/${res.data.file}`;
       clearInterval(timer);
       setProgress(100);
       setVttUrl(fullUrl);
